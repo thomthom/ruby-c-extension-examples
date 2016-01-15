7c192066e70e3a3e3e3af2a95d05e15a4dfe2c54
Thomas Thomassen <thomas@thomthom.net>
2016-01-14 23:05:21 -0700

    Empty Ruby extension that is more version agnostic.

--- Hello World/src/SUEX_HelloWorld.cpp
+++ Hello World/src/SUEX_HelloWorld.cpp
@@ -1,18 +1,4 @@
-
-#include "RubyUtils/RubyUtils.h"
-
-
-VALUE hello_world() {
-  return GetRubyInterface("Hello World!");
-}
-
-// Load this module from Ruby using:
-//   require 'SUEX_HelloWorld'
 extern "C"
-void Init_SUEX_HelloWorld()
+void Init_Empty()
 {
-  VALUE mSUEX_HelloWorld = rb_define_module("SUEX_HelloWorld");
-  rb_define_const(mSUEX_HelloWorld, "CEXT_VERSION", GetRubyInterface("1.0.0"));
-  rb_define_module_function(mSUEX_HelloWorld, "hello_world",
-    VALUEFUNC(hello_world), 0); 
 }
