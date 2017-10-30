3b59fbb3466a20e84e48301dd1723cbfc62796c7
Thomas Thomassen <thomas@thomthom.net>
2017-10-30 16:06:10 +0100

    Test thread id.

--- Hello World/src/SUEX_HelloWorld.cpp
+++ Hello World/src/SUEX_HelloWorld.cpp
@@ -2,6 +2,17 @@
 #include "RubyUtils/RubyUtils.h"
 
 
+VALUE current_thread() {
+  VALUE thread = rb_thread_current();
+  return rb_obj_id(thread);
+}
+
+VALUE current_fiber() {
+  VALUE fiber = rb_fiber_current();
+  return rb_obj_id(fiber);
+}
+
+
 VALUE hello_world() {
   return GetRubyInterface("Hello World!");
 }
@@ -13,6 +24,7 @@ void Init_SUEX_HelloWorld()
 {
   VALUE mSUEX_HelloWorld = rb_define_module("SUEX_HelloWorld");
   rb_define_const(mSUEX_HelloWorld, "CEXT_VERSION", GetRubyInterface("1.0.0"));
-  rb_define_module_function(mSUEX_HelloWorld, "hello_world",
-    VALUEFUNC(hello_world), 0); 
+  rb_define_module_function(mSUEX_HelloWorld, "hello_world", VALUEFUNC(hello_world), 0);
+  rb_define_module_function(mSUEX_HelloWorld, "current_thread", VALUEFUNC(current_thread), 0);
+  rb_define_module_function(mSUEX_HelloWorld, "current_fiber", VALUEFUNC(current_fiber), 0);
 }
