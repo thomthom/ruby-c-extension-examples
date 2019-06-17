8414c452a1d20222f5476c12dc60b2454bd2ccbe
Thomas Thomassen <thomas@thomthom.net>
2019-06-17 12:07:45 +0200

    Update notes

--- example.rb
+++ example.rb
@@ -2,4 +2,17 @@ require 'SUEX_HelloWorld'
 
 SUEX_HelloWorld.hello_world
 
+
+require 'SUEX_HelloWorld'
+
 SUEX_HelloWorld.is_section_active?
+
+
+=begin
+
+Add framework search path:
+/Applications/SketchUp 2018/SketchUp.app/Contents/MacOS/SketchUp
+
+Add SketchUpApi.framework to Xcode project, but don't link
+
+=begin
