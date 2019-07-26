2031c701fd6470a633bd6bc8c574236c959278da
Thomas Thomassen <thomas@thomthom.net>
2019-07-26 11:36:28 +0200

    SUFaceIsComplex

--- Hello World/src/SUEX_HelloWorld.cpp
+++ Hello World/src/SUEX_HelloWorld.cpp
@@ -9,7 +9,6 @@
 #include "SketchUpAPI/sketchup.h"
 #else
 #import <SketchUpAPI/sketchup.h>
-//#import "SketchUpAPI/sketchup.h"
 #endif
 
 
@@ -21,9 +20,9 @@ assert(SU_ERROR_NONE == su_api_result);\
 #define refute(condition) assert(!(condition))
 
 
-extern "C" {
-SU_RESULT SUApplicationGetActiveModel(SUModelRef* model);
-}
+//extern "C" {
+//SU_RESULT SUApplicationGetActiveModel(SUModelRef* model);
+//}
 
 
 std::string GetString(const SUStringRef& string) {
@@ -37,13 +36,13 @@ std::string GetString(const SUStringRef& string) {
 }
 
 SUModelRef GetActiveModel() {
-  VALUE sketchup = rb_define_module("Sketchup");
-  VALUE model = rb_funcall(sketchup, rb_intern("active_model"), 0);
-  VALUE address = rb_funcall(model, rb_intern("skpdoc"), 1, Qtrue);
-  return SUModelFromExisting(NUM2SIZET(address));
-  //SUModelRef model = SU_INVALID;
-  //SU(SUApplicationGetActiveModel(&model));
-  //return model;
+  //VALUE sketchup = rb_define_module("Sketchup");
+  //VALUE model = rb_funcall(sketchup, rb_intern("active_model"), 0);
+  //VALUE address = rb_funcall(model, rb_intern("skpdoc"), 1, Qtrue);
+  //return SUModelFromExisting(NUM2SIZET(address));
+  SUModelRef model = SU_INVALID;
+  SU(SUApplicationGetActiveModel(&model));
+  return model;
 }
 
 
@@ -75,9 +74,86 @@ VALUE is_section_active() {
   return Qnil;
 }
 
+VALUE is_face_complex() {
+  auto model = GetActiveModel();
+
+  SUEntitiesRef root_entities = SU_INVALID;
+  SU(SUModelGetEntities(model, &root_entities));
+
+  size_t num_groups = 0;
+  SU(SUEntitiesGetNumGroups(root_entities, &num_groups));
+  if (num_groups == 0)
+    return Qnil;
+
+  SUGroupRef group = SU_INVALID;
+  SU(SUEntitiesGetGroups(root_entities, 1, &group, &num_groups));
+
+  SUEntitiesRef entities = SU_INVALID;
+  SU(SUGroupGetEntities(group, &entities));
+
+  size_t num_faces = 0;
+  SU(SUEntitiesGetNumFaces(entities, &num_faces));
+
+  std::vector<SUFaceRef> faces(num_faces, SU_INVALID);
+  if (num_faces > 0) {
+    SU(SUEntitiesGetFaces(entities, num_faces, faces.data(), &num_faces));
+  }
+
+  if (faces.size() > 0) {
+    auto face = faces[0];
+    bool is_complex = false;
+    SUFaceIsComplex(face, &is_complex);
+    return is_complex ? Qtrue : Qfalse;
+  }
+
+  return Qnil;
+}
+
+VALUE num_triangles() {
+  auto model = GetActiveModel();
+
+  SUEntitiesRef root_entities = SU_INVALID;
+  SU(SUModelGetEntities(model, &root_entities));
+
+  size_t num_groups = 0;
+  SU(SUEntitiesGetNumGroups(root_entities, &num_groups));
+  if (num_groups == 0)
+    return Qnil;
+
+  SUGroupRef group = SU_INVALID;
+  SU(SUEntitiesGetGroups(root_entities, 1, &group, &num_groups));
+
+  SUEntitiesRef entities = SU_INVALID;
+  SU(SUGroupGetEntities(group, &entities));
+
+  size_t num_faces = 0;
+  SU(SUEntitiesGetNumFaces(entities, &num_faces));
+
+  std::vector<SUFaceRef> faces(num_faces, SU_INVALID);
+  if (num_faces > 0) {
+    SU(SUEntitiesGetFaces(entities, num_faces, faces.data(), &num_faces));
+  }
+
+  if (faces.size() > 0) {
+    auto face = faces[0];
+
+    SUMeshHelperRef mesh = SU_INVALID;
+    SU(SUMeshHelperCreate(&mesh, face));
+
+    size_t num_triangles = 0;
+    SU(SUMeshHelperGetNumTriangles(mesh, &num_triangles));
+
+    return SIZET2NUM(num_triangles);
+  }
+
+  return Qnil;
+}
+
 // Load this module from Ruby using:
 //   require 'SUEX_HelloWorld'
 //   SUEX_HelloWorld.is_section_active?
+//   SUEX_HelloWorld.is_face_complex?
+//   SUEX_HelloWorld.num_triangles
 extern "C"
 void Init_SUEX_HelloWorld()
 {
@@ -90,4 +166,10 @@ void Init_SUEX_HelloWorld()
   
   rb_define_module_function(mSUEX_HelloWorld, "is_section_active?",
                             VALUEFUNC(is_section_active), 0);
+
+  rb_define_module_function(mSUEX_HelloWorld, "is_face_complex?",
+                            VALUEFUNC(is_face_complex), 0);
+
+  rb_define_module_function(mSUEX_HelloWorld, "num_triangles",
+                            VALUEFUNC(num_triangles), 0);
 }
